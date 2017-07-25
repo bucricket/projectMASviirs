d8053314621e18c88b5aa39bffa51c7090981636
bucricket <bucricket@gmail.com>
2017-07-25 19:00:06 -0400

    Changed minor things

--- source/atmos_corr/calc_offset_correction.f90
+++ source/atmos_corr/calc_offset_correction.f90
@@ -1,5 +1,5 @@
 program calc_offset_correction
-character (len=:), allocatable :: basedir
+character (len=*), allocatable :: basedir
 character(len=4) :: tile_num
 integer, parameter :: dx=3750, dy=3750
 character(len=255) :: dayfile, nightfile, dtimefile, ntimefile
