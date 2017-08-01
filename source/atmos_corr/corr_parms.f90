687f458c5879afcfc199c7de2238a0bc3af6d278
bucricket <bucricket@gmail.com>
2017-08-01 10:23:14 -0400

    Fixed a strange file size issue

--- source/atmos_corr/corr_parms.f90
+++ source/atmos_corr/corr_parms.f90
@@ -4,7 +4,8 @@ integer, parameter :: ilg=3750      ! x-grid points
 integer, parameter :: jlg=3750     ! y-grid points
 integer, parameter :: kz=21        ! number of vertical levels
 
-integer, parameter :: ci=2880, cj=1200
+!integer, parameter :: ci=2880, cj=1200 ! original from CH
+integer, parameter :: ci=720, cj=1440
 
 
 
