bdd55be94ab4f4748e249b62b0bd8373653fde43
bucricket <bucricket@gmail.com>
2017-09-09 09:30:32 -0400

    Changed the parameters file

--- source/atmos_corr/corr_parms.f90
+++ source/atmos_corr/corr_parms.f90
@@ -4,8 +4,8 @@ integer, parameter :: ilg=3750      ! x-grid points
 integer, parameter :: jlg=3750     ! y-grid points
 integer, parameter :: kz=21        ! number of vertical levels
 
-integer, parameter :: ci=2880, cj=1200 ! original from CH
-!integer, parameter :: ci=1440, cj=720
+!integer, parameter :: ci=2880, cj=1200 ! original from CH
+integer, parameter :: ci=1440, cj=720
 
 
 
